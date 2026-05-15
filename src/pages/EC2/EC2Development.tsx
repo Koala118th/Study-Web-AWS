8c5f5eb8abeca23df91c4d69e74796483152024d
pquangw <quangnp2005@gmail.com>
2026-05-15 13:38:52 +0700

    fix: delete dark mode

--- src/pages/EC2/EC2Development.tsx
+++ src/pages/EC2/EC2Development.tsx
@@ -2,7 +2,7 @@ import "./EC2.css";
 import EC2Header from "./EC2Header";
 export default function S3Overview() {
     return (
-        <div>
+        <div className="ec2-page">
             <EC2Header></EC2Header>
         </div>
     )
